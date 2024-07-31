1f5668123fe09cdb5bf8a732898a8998008c1d3e
Marcy <marcelly.farias@fatec.sp.gov.br>
2024-07-31 14:52:21 -0300

    criando mutations

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,7 +2,10 @@ const userResolvers = {
   Query: {
     // context  //info
     users: (root, args, { dataSources }) => dataSources.UsersAPI.getUsers(),
-    user: (root, {id}, { dataSources }) => dataSources.UsersAPI.getUserById(id)
+    user: (root, { id }, { dataSources }) => dataSources.UsersAPI.getUserById(id)
+},
+  Mutation: {
+    adicionaUser: (root, user, { dataSources }) => dataSources.UsersAPI.adicionaUser(user),
   },
 };
 
