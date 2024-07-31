32388a51d22a40de5bb1c14f4885ef02ab40a88c
Marcy <marcelly.farias@fatec.sp.gov.br>
2024-07-31 15:31:12 -0300

    adicionando, alterando e deletando dados

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -2,11 +2,17 @@ const userResolvers = {
   Query: {
     // context  //info
     users: (root, args, { dataSources }) => dataSources.UsersAPI.getUsers(),
+
     user: (root, { id }, { dataSources }) => dataSources.UsersAPI.getUserById(id)
 },
   Mutation: {
-    adicionaUser: (root, user, { dataSources }) => dataSources.UsersAPI.adicionaUser(user),
-  },
-};
+    adicionaUser: async (root, user, { dataSources }) => dataSources.UsersAPI.adicionaUser(user),
+
+    atualizaUser: async (root, novosDados, { dataSources }) => dataSources.UsersAPI.atualizaUser(novosDados),
+
+    deletaUser: async (root, {id}, {dataSources}) => dataSources.UsersAPI.deletaUser(id)
+
+    }
+  };
 
 module.exports = userResolvers;
