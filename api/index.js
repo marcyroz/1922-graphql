dadc843626d94d8255b35441d715ef26d01d9b63
Marcy <marcelly.farias@fatec.sp.gov.br>
2024-07-31 11:59:52 -0300

    conectando com dados

--- api/index.js
+++ api/index.js
@@ -4,11 +4,18 @@
 const { ApolloServer } = require("apollo-server");
 const userSchema = require("./user/schema/user.graphql");
 const userResolvers = require("./user/resolvers/userResolvers");
+const UsersAPI = require("./user/datasource/user");
 
 const typeDefs = [userSchema];
 const resolvers = [userResolvers];
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return { UsersAPI: new UsersAPI() };
+  },
+});
 
 //Passando url, ele vai subir por padrão na porta 4000
 server.listen().then(({ url }) => {
