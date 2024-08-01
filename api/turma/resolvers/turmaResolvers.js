e1d84e0c51cf64796143812632e503132a92ef5d
Marcy <marcelly.farias@fatec.sp.gov.br>
2024-08-01 16:03:05 -0300

    adicionando demais querys e mutations

--- api/turma/resolvers/turmaResolvers.js
+++ api/turma/resolvers/turmaResolvers.js
@@ -1,6 +1,22 @@
+const { GraphQLScalarType } = require("graphql");
+
 const turmaResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "string de data e hora no formato ISO-8601",
+    serialize: (value) => new Date(value).toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value).toISOString(),
+  }),
+
   Query: {
     turmas: (_, __, { dataSources }) => dataSources.TurmasAPI.getTurmas(),
+    turma: (_, { id }, { dataSources }) => dataSources.TurmasAPI.getTurma(id),
+  },
+  Mutation: {
+    incluiTurma: (_, { turma }, { dataSources }) => dataSources.TurmasAPI.incluiTurma(turma),
+    atualizaTurma: (_, novosDados, { dataSources }) => dataSources.TurmasAPI.atualizaTurma(novosDados),
+    deletaTurma: (_, { id }, { dataSources }) => dataSources.TurmasAPI.deletaTurma(id),
   },
 };
 
